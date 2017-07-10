6ea6927253ec90404af32fe98939800cf46f3b59
ByteThis <casmiguelf@gmail.com>
2017-07-10 19:59:27 +0100

    Password hashing added
    Using BCrypt.js for password hashing

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var multer = require('multer');
 var upload = multer({dest: './uploads'});
 var flash = require('connect-flash');
 var mongo = require('mongodb');
+var bcrypt = require('bcryptjs');
 var mongoose = require('mongoose');
 var db = mongoose.connection;
 
