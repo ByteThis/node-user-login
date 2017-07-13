ea6b327f3a3345d320fc4c8c2a93054392f7ded3
ByteThis <casmiguelf@gmail.com>
2017-07-13 17:32:21 +0100

    Working login
    Working login using passport.js

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ var bcrypt = require('bcryptjs');
 var mongoose = require('mongoose');
 var db = mongoose.connection;
 
-
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -27,8 +25,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -36,7 +32,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 
 //Handls Sessions
